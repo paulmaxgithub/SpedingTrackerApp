261f43d5941171c993be322a109f6a0d32921672
paulmax <ipaulmax@icloud.com>
2023-01-18 02:00:15 +0300

    Commit_#3: Card Form Pickers modified:	AddCardFormView.swift 		• created - fileprivate enum CardType 		• created - @State private variables to store card's data state 		• created - Form() with textfields & pickers to add card params

--- SpedingTrackerApp/Screens/MainSubViews/AddCardFormView.swift
+++ SpedingTrackerApp/Screens/MainSubViews/AddCardFormView.swift
@@ -7,16 +7,51 @@
 
 import SwiftUI
 
+fileprivate enum CardType: String, CaseIterable {
+    case Visa, Mastercard, Discover, Citibank
+}
+
 struct AddCardFormView: View {
     
     @Environment(\.dismiss) var dismiss
     
-    @State private var name = ""
+    @State private var name       = ""
+    @State private var cardNumber = ""
+    @State private var cardLimit  = ""
+    @State private var cardType: CardType = .Visa
+    
+    @State private var month = 1
+    @State private var year = Calendar.current.component(.year, from: Date())
+    
+    @State private var color: Color = .blue
     
     var body: some View {
         NavigationView {
             Form {
-                TextField("Name", text: $name)
+                
+                Section("CARD INFORMATION") {
+                    TextField("Name", text: $name)
+                    TextField("Credit Card Number", text: $cardNumber)
+                        .keyboardType(.numberPad)
+                    TextField("Card Limit", text: $cardLimit)
+                        .keyboardType(.numberPad)
+                    Picker("Type", selection: $cardType) {
+                        ForEach(CardType.allCases, id: \.self) { Text("\($0.rawValue)") }
+                    }
+                }
+                
+                Section("EXPIRATION") {
+                    Picker("Month", selection: $month) {
+                        ForEach(1...12, id: \.self) { Text("\($0)") }
+                    }
+                    Picker("Year", selection: $year) {
+                        ForEach(year..<year + 20, id: \.self) { Text(String($0)) }
+                    }
+                }
+                
+                Section("COLOR") {
+                    ColorPicker("Color", selection: $color)
+                }
             }
             .navigationTitle("Add Credit Card")
             .navigationBarItems(leading: Button(action: {
